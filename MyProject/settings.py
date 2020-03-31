52e26f205a67cefd21c3709115bc1e2aef5575a7
Chidambar Joshi <35500446+chidambarjoshi@users.noreply.github.com>
2020-03-31 15:06:56 +0530

    Initial commit

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import mimetypes
 
-mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".css", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
