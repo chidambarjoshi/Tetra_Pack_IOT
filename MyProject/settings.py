04e9ab26fa6d43fe0a7cfc02a7786dd0c157ee9b
Chidambar Joshi <35500446+chidambarjoshi@users.noreply.github.com>
2020-03-31 11:46:17 +0530

    Initial commit

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import mimetypes
 import tertaapp
+
+import django_heroku
+django_heroku.settings(False)
 mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
